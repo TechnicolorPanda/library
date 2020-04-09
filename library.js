d414e77da96678570f821dcd863e16a1893fb60a
Lorri Romesberg <LorriAnn7@att.net>
2020-04-08 21:35:53 -0400

    combined results into a single table

--- library.js
+++ library.js
@@ -1,3 +1,4 @@
+let rowNumber = -1;
 render("Title", "Author", "Pages", "Read");
 
 function Book(title, author, pages, read) {
@@ -17,19 +18,25 @@ function addBookToLibrary() {
 }
 
 function render() {
+    rowNumber++;
+    console.log(rowNumber);
     const box = document.getElementById("box");
-    const table = document.createElement("table");
-    const tableBody = document.createElement("tableBody");
+    const table = document.getElementById("bookTable");
+    const button = document.createElement("button");
+    button.innerHTML = "remove";
     let row = table.insertRow(0);
-    for(let j = 0; j < 4; j++){ 
+        for(let j = 0; j < 4; j++){ 
         let cell = document.createElement("td");
         cell.innerHTML = (arguments[j]);
         row.appendChild(cell);
-    }
-    tableBody.appendChild(row);
-    table.appendChild(tableBody); 
+        }
+    row.dataset.row = rowNumber;
+        if (rowNumber > 0) {
+            row.appendChild(button);
+        };
+    console.log(row);
+    table.appendChild(row);
     box.appendChild(table);
-    table.setAttribute("border", "2");
 }
 
 
