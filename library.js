de3c2a8e88d30770c13c2a521c6ae9497f234179
Lorri Romesberg <LorriAnn7@att.net>
2020-04-23 16:37:02 -0400

    added local storage

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 createHeading("Title", "Author", "Pages", "Read");
+testLocalStorage();
 
 function createHeading() {
     const box = document.getElementById("box");
@@ -28,6 +29,11 @@ function addBookToLibrary() {
     let read = document.getElementById("read").value;
     const myBook = new Book(title, author, pages, read);
     myLibrary.push(myBook);
+    //myLibrary.forEach(element => 
+        //render(element.title, element.author, element.pages, element.read));
+    myLibrary.forEach(element => 
+        mySavedLibrary = JSON.stringify(element.title + "," + element.author + "," + element.pages + "," + element.read));
+        localStorage.setItem("mySavedLibrary", mySavedLibrary);
     clearTable();
     myLibrary.forEach(element => 
         render(element.title, element.author, element.pages, element.read));
@@ -42,7 +48,9 @@ function render() {
             cell.innerHTML = (arguments[j]);
             row.appendChild(cell);
         }
+
     //create read checkbox on table
+
     const checkbox = document.createElement("input");
     checkbox.setAttribute("type","checkbox");
     if (arguments[3] === "yes"){
@@ -67,7 +75,7 @@ function clearTable() {
 }
 
 function removeBook(button) {
-    button.addEventListener("click", function(e) {
+    button.addEventListener("click", function() {
         for (let i = myLibrary.length-1; i>=0; i--) {
             if (myLibrary[i].title === button.value)
                 myLibrary.splice(i,1);
@@ -77,3 +85,68 @@ function removeBook(button) {
             render(element.title, element.author, element.pages, element.read));
         }, false);
 }
+
+//saves data to local storage
+
+function testLocalStorage() {
+    //let localStorateExists = true;
+    if (storageAvailable("localStorage")) {
+        if(!localStorage.getItem("mySavedLibrary")) {
+            console.log("Populate storage");
+            populateStorage();
+          } else {
+            console.log("Retrieve storage");
+            retrieveStorage();
+          }
+      }
+      else {
+        //let localStorageExists = false;
+        console.log("Library can not be saved.")
+      }
+}
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        let x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            e.code === 22 ||
+            e.code === 1014 ||
+            e.name === 'QuotaExceededError' ||
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            (storage && storage.length !== 0);
+    }
+}
+
+function populateStorage() {
+        //myLibrary =[]
+        let storageLength = localStorage.length;
+        for(i=0; i<storageLength+1; i++){
+            const arr = ["Harry Potter", "JK Rowlings", "343","yes"];
+            myLibrary[i] = new Book(arr[0],arr[1],arr[2],arr[3]);
+        }
+
+        myLibrary.forEach(element => 
+            mySavedLibrary = JSON.stringify(element.title + "," + element.author + "," + element.pages + "," + element.read));
+            localStorage.setItem("mySavedLibrary", mySavedLibrary);
+            clearTable();
+}
+
+function retrieveStorage() {
+    let libraryLength = localStorage.length;
+    for(i=0; i<libraryLength; i++){
+        const arr = localStorage.getItem("mySavedLibrary").split(",");
+        myLibrary[i] = new Book(arr[0],arr[1],arr[2],arr[3])
+        console.table(myLibrary);
+    }
+    myLibrary.forEach(element => 
+        render(element.title, element.author, element.pages, element.read));
+}
+
+
